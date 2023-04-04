569cea673611341118b2c85a12181b5fe3cb0db6
Thanhdat31 <125192189+Thanhdat31@users.noreply.github.com>
2023-04-04 14:39:14 +0700

    Update CTDL&GT_Selection_Sort_1.cpp

--- CTDL&GT_Selection_Sort_1.cpp
+++ CTDL&GT_Selection_Sort_1.cpp
@@ -2,12 +2,12 @@ https://codelearn.io/sharing/cac-thuat-toan-sap-xep-trong-cpp
 
 void SelectionSort(int a[], int n)
 {
-	int min; // ch? s? ph?n t? nh? nh?t trong dãy hi?n hành
+	int min; // 
 	for (int i = 0; i < n - 1; i++) {
 		min = i;
 		for (int j = i + 1; j < n; j++)
 			if (a[j] < a[min])
-				min = j; // ghi nh?n v? trí ph?n t? nh? nh?t
+				min = j; 
 		if (min != i)
 			Swap(a[min], a[i]);
 	}
