8929fbaf4a353bb81c05a0efb1a3d7e8c4c9aafc
Thanhdat31 <125192189+Thanhdat31@users.noreply.github.com>
2023-04-04 17:00:21 +0700

    Add files via upload

---
+++ CTDL&GT_Exponential_Recursion_2.cpp
@@ -0,0 +1,37 @@
+﻿#include<stdio.h>
+#include<iostream>
+using namespace std;
+void dayNhiPhan(int i, int n, int* X)
+{
+    int val;    
+    for (val = 0; val < 2; val++)      
+    {
+        X[i] = val;
+
+        if (i == (n - 1))         
+        {
+            int j;
+            for (j = 0; j < n; j++)     
+            {
+                cout << X[j];
+            }
+            cout << "\n";
+        }
+
+        else              
+        {
+            dayNhiPhan(i + 1, n, X); 
+        }
+    }
+}
+
+int main()
+{
+    int n;
+    cout << "Nhập n : ";
+    cin >> n;
+
+    int X[n];
+    cout << "Dãy nhị phân có độ dài " << n << " là:\n";
+    dayNhiPhan(0, n, X);
+}
