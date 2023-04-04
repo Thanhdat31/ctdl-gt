898b1ef1b687ba08734167506f7a6afe617cf21f
Thanhdat31 <125192189+Thanhdat31@users.noreply.github.com>
2023-04-04 14:08:17 +0700

    Add files via upload

---
+++ CTDL&GT_Interchange_Sort_1.cpp
@@ -0,0 +1,12 @@
+﻿void Swap(int& a, int& b) {
+    int temp = a;
+    a = b;
+    b = temp;
+}
+
+void InterchangeSort(int a[], int n) {
+    for (int i = 0; i < n - 1; i++)
+        for (int j = i + 1; j < n; j++)
+            if (a[i] > a[j]) 
+                Swap(a[i], a[j]);
+}
