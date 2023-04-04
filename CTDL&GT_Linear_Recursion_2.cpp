98913d1db9b0b093b0fa4b2f27656e794eb23dd4
Thanhdat31 <125192189+Thanhdat31@users.noreply.github.com>
2023-04-04 16:46:11 +0700

    Add files via upload

---
+++ CTDL&GT_Linear_Recursion_2.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+using namespace std;
+
+int factorial(int n) {
+	if (n == 0) return 1; 
+	return n * factorial(n - 1);
+}
+//hàm main
+int main() {
+	int n;
+	cout << "nhap so : ";
+	cin >> n;
+	int kq = factorial(n);
+	cout << "\nKết quả \n" << n << "! = " << kq;
+
+}
